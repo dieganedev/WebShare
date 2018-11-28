2a2e8c15aacf418cc1ef383be174b961586c2809
dieganedev <dioufmamediegane@gmail.com>
2018-11-28 16:07:46 +0000

    message teste

--- WebShare/prod_settings.py
+++ WebShare/prod_settings.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-DATABASE['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = '0eg50#9m3cz44#-vdk)u+q_vmkfwy)9psoe@m8k901mra3zim#'
 
