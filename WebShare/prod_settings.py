de3e5ec8b4f86d3e23b82f78633f0e0ebccbd225
dieganedev <dioufmamediegane@gmail.com>
2018-11-28 15:08:00 +0000

    message teste

--- WebShare/prod_settings.py
+++ WebShare/prod_settings.py
@@ -6,4 +6,4 @@ TEMPLATE_DEBUG = False
 
 SECRET_KEY = '0eg50#9m3cz44#-vdk)u+q_vmkfwy)9psoe@m8k901mra3zim#'
 
-ALLOWEDS_HOST = ['app-webshare.herokuapp.com']
+ALLOWED_HOSTS = ['app-webshare.herokuapp.com']
