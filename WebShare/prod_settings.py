0de3c0da1980be7b2d7202547ad7ff35aec6093c
dieganedev <dioufmamediegane@gmail.com>
2018-11-28 13:04:08 +0000

    Add productions settings

---
+++ WebShare/prod_settings.py
@@ -0,0 +1,9 @@
+from WebShare.sittings import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = '0eg50#9m3cz44#-vdk)u+q_vmkfwy)9psoe@m8k901mra3zim#'
+
+ALLOWEDS_HOST = ['app-webshare.herokuapp.com']
